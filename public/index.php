d571c4da13443dada8b7759f5b899397b8904bdc
Alexandre Tavares <alexandre@providentcrm.com>
2018-04-20 14:03:10 +0100

    Add htaccess file

--- public/index.php
+++ public/index.php
@@ -6,3 +6,4 @@ require_once(__DIR__.'/../src/Hello.php');
 $hello = new \Alex\Demo\Hello();
 
 echo $hello->world();
+
--- src/Hello.php
+++ src/Hello.php
@@ -8,4 +8,19 @@ class Hello
     {
         return 'world';
     }
+    public function get_things( $x ) {
+
+        $l = array();
+
+        for ( $i = 0; $i < count( $x ); $i++ ) {
+
+            if ( true === $x[ $i ] ) {
+                array_push( $l, $x[ $i ] );
+            }
+
+        }
+
+        return $l;
+
+    }
 }
