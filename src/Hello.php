a76c46ee9ac5f55d403adab75c711bee16c67e5b
Alexandre Tavares <alexandre@providentcrm.com>
2018-04-23 10:29:16 +0100

    Remove method

--- src/Hello.php
+++ src/Hello.php
@@ -8,19 +8,5 @@ class Hello
     {
         return 'world';
     }
-    public function get_things( $x ) {
-
-        $l = array();
-
-        for ( $i = 0; $i < count( $x ); $i++ ) {
-
-            if ( true === $x[ $i ] ) {
-                array_push( $l, $x[ $i ] );
-            }
-
-        }
-
-        return $l;
-
-    }
+    
 }
